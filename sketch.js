66af6b26e02760eac7e34edc4ea3f4c6e30032f7
mohosm <mm7134@nyu.edu>
2016-05-02 01:06:07 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -135,7 +135,7 @@ function Hives(){
   this.yHive=mouseY;
   
   this.displayHive = function(){
-    image(hive1,this.xHive,this.yHive,50,65);
+    image(hive1,this.xHive,this.yHive,70,100);
   }
 }
 
