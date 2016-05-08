3bcfb0e37939b4dd7e9b79adfede3d1cafa27654
mohosm <mm7134@nyu.edu>
2016-05-08 21:17:23 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -205,7 +205,7 @@ function Bee() {
     this.rot = PI;
     if(this.xLeader > 0){this.xDirNeg = true}else{this.xDirNeg = false };
     if(this.yLeader > 0){this.yDirNeg = true}else{this.yDirNeg = false };
-    this.r1 = floor(random(firstnames.length));
+    this.r1 = floor(random(100));
     this.r2 = floor(random(nicknames.length));
     this.r3 = floor(random(lastnames.length));
     this.r4 = floor(random(s1s.length));
