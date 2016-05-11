c83ac1926d4e9a8469a22619ee1075385932bae7
mohosm <mm7134@nyu.edu>
2016-05-11 14:18:37 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -115,6 +115,7 @@ function mouseClicked(){
   for (var i = 0; i < allBees.length; i = i+1) {
     if(mouseX<allBees[i].xCor+50 && mouseX>allBees[i].xCor-30 &&mouseY<allBees[i].yCor+50 && mouseY>allBees[i].yCor-30 && inspectorBoo == true){
     allBees[i].selected = true;
+    i=allBees.length;
     texy = allBees[i].fn +" "+'"'+allBees[i].nn+'"'+" "+allBees[i].ln+"\n"+allBees[i].s11+" "+allBees[i].s22+" "+allBees[i].s33;
     } else {
     allBees[i].selected = false;
